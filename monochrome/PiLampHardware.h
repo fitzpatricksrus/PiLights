8723962e8edf894bef61bfd43dd205bd24c7822b
fitzpatricksrus <junk@fitzpatircksr.us>
2019-09-05 21:21:36 -0700

    Another attempt.  Cauz templates.

--- monochrome/PiLampHardware.h
+++ monochrome/PiLampHardware.h
@@ -6,6 +6,8 @@
 #define PILIGHTS_PILAMPHARDWARE_H
 
 #include "MonoLampMatrixHardware.h"
+#include "pigpio.h"
+#include <algorithm>
 
 template <int rows, int cols>
 class PiLampHardware : public MonoLampMatrixHardware<rows, cols> {
@@ -24,5 +26,37 @@ private:
     std::array<int, rows> rowPins;
     std::array<int, cols> colPins;
 };
+
+template<int rows, int cols>
+PiLampHardware<rows, cols>::PiLampHardware(int rowPinsIn[], int colPinsIn[], bool activeHighIn)
+        :activeHigh(activeHigh), rowPins(), colPins()
+{
+    std::copy_n(&rowPinsIn[0], rows, rowPins.begin());
+    std::copy_n(&colPinsIn[0], cols, colPins.begin());
+
+/*    for (int i = 0; i < rows; i++) {
+        rowPins[i] = rowPinsIn[i];
+    }
+    for (int i = 0; i < cols; i++) {
+        colPins[i] = colPinsIn[i];
+    } */
+}
+
+template<int rows, int cols>
+void PiLampHardware<rows, cols>::refreshColumn(int row, const bool* values) {
+    for (int i = 0; i < rows; i++) {
+        setPin(rowPins[i], !activeHigh);
+    }
+    for (int i = 0; i < cols; i++) {
+        setPin(colPins[i], activeHigh == values[i]);
+    }
+    setPin(rowPins[row], activeHigh);
+}
+
+template<int rows, int cols>
+inline void PiLampHardware<rows, cols>::setPin(int pin, bool value) {
+    gpioWrite(pin, value);
+}
+
 
 #endif //PILIGHTS_PILAMPHARDWARE_H
